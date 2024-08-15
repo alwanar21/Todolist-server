b49d9c3c50955363ce4c455722a96a71729c2e85
Alwan AR <alwanar210801@gmail.com>
2024-08-15 21:30:09 +0700

    feat : add task model

---
+++ prisma/migrations/20240813130048_add/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `tasks` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nama` VARCHAR(20) NOT NULL,
+    `description` VARCHAR(100) NULL,
+    `date` DATETIME(3) NOT NULL,
+    `status` ENUM('PENDING', 'DOING', 'DONE') NOT NULL DEFAULT 'DOING',
+    `kelompok` ENUM('WORK', 'PERSONAL', 'SHOPPING', 'OTHERS') NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `tasks` ADD CONSTRAINT `tasks_username_fkey` FOREIGN KEY (`username`) REFERENCES `users`(`username`) ON DELETE RESTRICT ON UPDATE CASCADE;
